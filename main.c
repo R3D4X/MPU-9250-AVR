2fc4c84d6b58eabcd6b8e538ddd642573e23329e
R3D4X <49124387+R3D4X@users.noreply.github.com>
2019-03-31 17:30:55 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,183 @@
+//#include <Wire.h>
+#include <avr/io.h>
+
+#include "i2cmaster.h"
+#include "mpu9250.h"
+#include "uart.h"
+
+int16_t accelCount[3];  // Stores the 16-bit signed accelerometer sensor output
+int16_t gyroCount[3];   // Stores the 16-bit signed gyro sensor output
+int16_t magCount[3];    // Stores the 16-bit signed magnetometer sensor output
+
+float ax, ay, az, gx, gy, gz, mx, my, mz; // variables to hold latest sensor data values 
+float q[4] = {1.0f, 0.0f, 0.0f, 0.0f};    // vector to hold quaternion
+float eInt[3] = {0.0f, 0.0f, 0.0f};       // vector to hold integral error for Mahony method
+
+uint32_t lastUpdate = 0; // used to calculate integration interval
+uint32_t Now = 0;        // used to calculate integration interval
+float deltat = 0.0f, sum = 0.0f;        // integration interval for both filter schemes
+float pitch, yaw, roll;
+uint32_t delt_t = 0; // used to control display output rate
+uint32_t count = 0, sumCount = 0; // used to control display output rate
+
+//void setup()
+void main()
+{
+	//Wire.begin();
+    	i2c_init();                                // init I2C interface
+  	//Serial.begin(115200);
+	UART_Init(115200);
+
+  	mpu9250_setup();
+
+	//void loop()
+	while(1)
+	{  
+  		// If intPin goes high, all data registers have new data
+  		if (readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01) 
+		{  // On interrupt, check if data ready interrupt
+    			readAccelData(accelCount);  // Read the x/y/z adc values
+    			getAres();
+    
+    			// Now we'll calculate the accleration value into actual g's
+    			ax = (float)accelCount[0]*aRes; // - accelBias[0];  // get actual g value, this depends on scale being set
+    			ay = (float)accelCount[1]*aRes; // - accelBias[1];   
+    			az = (float)accelCount[2]*aRes; // - accelBias[2];  
+   
+    			readGyroData(gyroCount);  // Read the x/y/z adc values
+    			getGres();
+ 
+    			// Calculate the gyro value into actual degrees per second
+    			gx = (float)gyroCount[0]*gRes;  // get actual gyro value, this depends on scale being set
+    			gy = (float)gyroCount[1]*gRes;  
+    			gz = (float)gyroCount[2]*gRes;   
+  
+    			readMagData(magCount);  // Read the x/y/z adc values
+    			getMres();
+//    			magbias[0] = +470.;  // User environmental x-axis correction in milliGauss, should be automatically calculated
+//    			magbias[1] = +120.;  // User environmental x-axis correction in milliGauss
+//    			magbias[2] = +125.;  // User environmental x-axis correction in milliGauss
+    
+    			// Calculate the magnetometer values in milliGauss
+    			// Include factory calibration per data sheet and user environmental corrections
+    			mx = (float)magCount[0]*mRes*magCalibration[0] - magBias[0];  // get actual magnetometer value, this depends on scale being set
+    			my = (float)magCount[1]*mRes*magCalibration[1] - magBias[1];  
+    			mz = (float)magCount[2]*mRes*magCalibration[2] - magBias[2];   
+  		}
+  
+  		Now = micros();
+  		deltat = ((Now - lastUpdate)/1000000.0f); // set integration time by time elapsed since last filter update
+  		lastUpdate = Now;
+
+  		sum += deltat; // sum for averaging filter update rate
+  		sumCount++;
+  
+  		// Sensors x (y)-axis of the accelerometer is aligned with the y (x)-axis of the magnetometer;
+  		// the magnetometer z-axis (+ down) is opposite to z-axis (+ up) of accelerometer and gyro!
+  		// We have to make some allowance for this orientationmismatch in feeding the output to the quaternion filter.
+  		// For the MPU-9250, we have chosen a magnetic rotation that keeps the sensor forward along the x-axis just like
+  		// in the LSM9DS0 sensor. This rotation can be modified to allow any convenient orientation convention.
+  		// This is ok by aircraft orientation standards!  
+  		// Pass gyro rate as rad/s
+  		MadgwickQuaternionUpdate(ax, ay, az, gx*PI/180.0f, gy*PI/180.0f, gz*PI/180.0f,  my,  mx, mz);
+//  		MahonyQuaternionUpdate(ax, ay, az, gx*PI/180.0f, gy*PI/180.0f, gz*PI/180.0f, my, mx, mz);
+
+
+    		if (!AHRS) 
+		{
+    			delt_t = millis() - count;
+    			if(delt_t > 500) 
+			{
+    				if(SerialDebug) 
+				{
+    				// Print acceleration values in milligs!
+//    					UART_Printf("X-acceleration: "); UART_Printf(1000*ax); UART_Printf(" mg ");
+//    					UART_Printf("Y-acceleration: "); UART_Printf(1000*ay); UART_Printf(" mg ");
+//    					UART_Printf("Z-acceleration: "); UART_Printf(1000*az); UART_Printf(" mg "); UART_Printf("\n");
+// 
+//    				// Print gyro values in degree/sec
+//    					UART_Printf("X-gyro rate: "); UART_Printf(gx, 3); UART_Printf(" degrees/sec "); 
+//    					UART_Printf("Y-gyro rate: "); UART_Printf(gy, 3); UART_Printf(" degrees/sec "); 
+//    					UART_Printf("Z-gyro rate: "); UART_Printf(gz, 3); UART_Printf(" degrees/sec"); UART_Printf("\n");
+//    
+//    				// Print mag values in degree/sec
+//    					UART_Printf("X-mag field: "); UART_Printf(mx); UART_Printf(" mG "); 
+//    					UART_Printf("Y-mag field: "); UART_Printf(my); UART_Printf(" mG "); 
+//    					UART_Printf("Z-mag field: "); UART_Printf(mz); UART_Printf(" mG"); UART_Printf("\n");
+ 
+    					tempCount = readTempData();  // Read the adc values
+    					temperature = ((float) tempCount) / 333.87 + 21.0; // Temperature in degrees Centigrade
+   				// Print temperature in degrees Centigrade      
+    					UART_Printf("Temperature is %d in °/C\n",temperature, 1);
+    				}
+    
+    				count = millis();
+    				digitalWrite(myLed, !digitalRead(myLed));  // toggle led
+    			}
+    		}
+    		else 
+    		{
+      
+    			// UART_Printf and/or display at 0.5 s rate independent of data rates
+    			delt_t = millis() - count;
+    			if (delt_t > 50) 
+			{ // update LCD once per half-second independent of read rate
+
+    				if(SerialDebug) 
+				{
+//    					UART_Printf("ax = "); UART_Printf((int)1000*ax);  
+//    					UART_Printf(" ay = "); UART_Printf((int)1000*ay); 
+//    					UART_Printf(" az = "); UART_Printf((int)1000*az); UART_Printfln(" mg");
+//    					UART_Printf("gx = "); UART_Printf( gx, 2); 
+//    					UART_Printf(" gy = "); UART_Printf( gy, 2); 
+//    					UART_Printf(" gz = "); UART_Printf( gz, 2); UART_Printfln(" deg/s");
+//    					UART_Printf("mx = "); UART_Printf( (int)mx ); 
+//    					UART_Printf(" my = "); UART_Printf( (int)my ); 
+//    					UART_Printf(" mz = "); UART_Printf( (int)mz ); UART_Printfln(" mG");
+//    	
+//    					UART_Printf("q0 = "); UART_Printf(q[0]);
+//   	 				UART_Printf(" qx = "); UART_Printf(q[1]); 
+//   	 				UART_Printf(" qy = "); UART_Printf(q[2]); 
+//   	 				UART_Printf(" qz = "); UART_Printfln(q[3]); 
+    				}               
+    
+  				// Define output variables from updated quaternion---these are Tait-Bryan angles, commonly used in aircraft orientation.
+  				// In this coordinate system, the positive z-axis is down toward Earth. 
+  				// Yaw is the angle between Sensor x-axis and Earth magnetic North (or true North if corrected for local declination, looking down on the sensor positive yaw is counterclockwise.
+  				// Pitch is angle between sensor x-axis and Earth ground plane, toward the Earth is positive, up toward the sky is negative.
+  				// Roll is angle between sensor y-axis and Earth ground plane, y-axis up is positive roll.
+  				// These arise from the definition of the homogeneous rotation matrix constructed from quaternions.
+  				// Tait-Bryan angles as well as Euler angles are non-commutative; that is, the get the correct orientation the rotations must be
+  				// applied in the correct order which for this configuration is yaw, pitch, and then roll.
+  				// For more see http://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles which has additional links.
+  	  			yaw   = atan2(2.0f * (q[1] * q[2] + q[0] * q[3]), q[0] * q[0] + q[1] * q[1] - q[2] * q[2] - q[3] * q[3]);   
+  	  			pitch = -asin(2.0f * (q[1] * q[3] - q[0] * q[2]));
+  	  			roll  = atan2(2.0f * (q[0] * q[1] + q[2] * q[3]), q[0] * q[0] - q[1] * q[1] - q[2] * q[2] + q[3] * q[3]);
+  	  			pitch *= 180.0f / PI;
+  	  			yaw   *= 180.0f / PI; 
+    				yaw   -= 2.45; /* 2019-03-30	2° 45' E  changing by  0.083° E per year (+ve for west )*/
+    				roll  *= 180.0f / PI;
+     
+
+    				UART_Printf("Yaw, Pitch, Roll: %d , %d , %d\n" , yaw+180, pitch, roll);
+    
+  	  			// With these settings the filter is updating at a ~145 Hz rate using the Madgwick scheme and 
+  	  			// >200 Hz using the Mahony scheme even though the display refreshes at only 2 Hz.
+  	  			// The filter update rate is determined mostly by the mathematical steps in the respective algorithms, 
+  	  			// the processor speed (8 MHz for the 3.3V Pro Mini), and the magnetometer ODR:
+  	  			// an ODR of 10 Hz for the magnetometer produce the above rates, maximum magnetometer ODR of 100 Hz produces
+  	  			// filter update rates of 36 - 145 and ~38 Hz for the Madgwick and Mahony schemes, respectively. 
+  	  			// This is presumably because the magnetometer read takes longer than the gyro or accelerometer reads.
+  	  			// This filter update rate should be fast enough to maintain accurate platform orientation for 
+  	  			// stabilization control of a fast-moving robot or quadcopter. Compare to the update rate of 200 Hz
+  	  			// produced by the on-board Digital Motion Processor of Invensense's MPU6050 6 DoF and MPU9150 9DoF sensors.
+  	  			// The 3.3 V 8 MHz Pro Mini is doing pretty well!
+	
+	    			count = millis(); 
+	    			sumCount = 0;
+	    			sum = 0;    
+	
+			}
+		}
+	}
+}
---
+++ mpu9250.h
@@ -0,0 +1,134 @@
+
+/* MPU9250 Basic Example Code
+ by: Kris Winer
+ date: April 1, 2014
+ license: Beerware - Use this code however you'd like. If you 
+ find it useful you can buy me a beer some time.
+ 
+ Demonstrate basic MPU-9250 functionality including parameterizing the register addresses, initializing the sensor, 
+ getting properly scaled accelerometer, gyroscope, and magnetometer data out. Added display functions to 
+ allow display to on breadboard monitor. Addition of 9 DoF sensor fusion using open source Madgwick and 
+ Mahony filter algorithms. Sketch runs on the 3.3 V 8 MHz Pro Mini and the Teensy 3.1.
+ 
+ SDA and SCL should have external pull-up resistors (to 3.3V).
+ 10k resistors are on the EMSENSR-9250 breakout board.
+ 
+ Hardware setup:
+ MPU9250 Breakout --------- Arduino
+ VDD ---------------------- 3.3V
+ VDDI --------------------- 3.3V
+ SDA ----------------------- A4
+ SCL ----------------------- A5
+ GND ---------------------- GND
+ 
+ Note: The MPU9250 is an I2C sensor and uses the Arduino Wire library. 
+ Because the sensor is not 5V tolerant, we are using a 3.3 V 8 MHz Pro Mini or a 3.3 V Teensy 3.1.
+ We have disabled the internal pull-ups used by the Wire library in the Wire.h/twi.c utility file.
+ We are also using the 400 kHz fast I2C mode by setting the TWI_FREQ  to 400000L /twi.h utility file.
+ */
+
+#ifndef avb_mpu9250
+#define avb_mpu9250
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <Arduino.h>
+
+#include "mpu9250_registers.h"
+
+// Using NOKIA 5110 monochrome 84 x 48 pixel display
+// pin 9 - Serial clock out (SCLK)
+// pin 8 - Serial data out (DIN)
+// pin 7 - Data/Command select (D/C)
+// pin 5 - LCD chip select (CS)
+// pin 6 - LCD reset (RST)
+//Adafruit_PCD8544 display = Adafruit_PCD8544(9, 8, 7, 5, 6);
+
+// Using the MSENSR-9250 breakout board, ADO is set to 0 
+// Seven-bit device address is 110100 for ADO = 0 and 110101 for ADO = 1
+#define ADO 1
+#if ADO
+#define MPU9250_ADDRESS 0x68  // Device address when ADO = 1
+#else
+#define MPU9250_ADDRESS 0x68  // Device address when ADO = 0
+#define AK8963_ADDRESS 0x0C   //  Address of magnetometer
+#endif  
+
+#define AHRS true         // set to false for basic data read
+#define SerialDebug true   // set to true to get Serial output for debugging
+
+// Set initial input parameters
+enum Ascale {
+  AFS_2G = 0,
+  AFS_4G,
+  AFS_8G,
+  AFS_16G
+};
+
+enum Gscale {
+  GFS_250DPS = 0,
+  GFS_500DPS,
+  GFS_1000DPS,
+  GFS_2000DPS
+};
+
+enum Mscale {
+  MFS_14BITS = 0, // 0.6 mG per LSB
+  MFS_16BITS      // 0.15 mG per LSB
+};
+
+// Specify sensor full scale
+uint8_t Gscale = GFS_250DPS;
+uint8_t Ascale = AFS_2G;
+uint8_t Mscale = MFS_16BITS; // Choose either 14-bit or 16-bit magnetometer resolution
+uint8_t Mmode = 0x02;        // 2 for 8 Hz, 6 for 100 Hz continuous magnetometer data read
+float aRes, gRes, mRes;      // scale resolutions per LSB for the sensors
+
+float   temperature;    // Stores the real internal chip temperature in degrees Celsius
+float   SelfTest[6];    // holds results of gyro and accelerometer self test
+
+float magBias[3],magScale[3];
+
+float magCalibration[3] = {0, 0, 0}, magbias[3] = {0, 0, 0};  // Factory mag calibration and mag bias
+float gyroBias[3] = {0, 0, 0}, accelBias[3] = {0, 0, 0};      // Bias corrections for gyro and accelerometer
+int16_t tempCount;      // temperature raw count output
+
+// global constants for 9 DoF fusion and AHRS (Attitude and Heading Reference System)
+float GyroMeasError = PI * (40.0f / 180.0f);   // gyroscope measurement error in rads/s (start at 40 deg/s)
+float GyroMeasDrift = PI * (0.0f  / 180.0f);   // gyroscope measurement drift in rad/s/s (start at 0.0 deg/s/s)
+// There is a tradeoff in the beta parameter between accuracy and response speed.
+// In the original Madgwick study, beta of 0.041 (corresponding to GyroMeasError of 2.7 degrees/s) was found to give optimal accuracy.
+// However, with this value, the LSM9SD0 response time is about 10 seconds to a stable initial quaternion.
+// Subsequent changes also require a longish lag time to a stable output, not fast enough for a quadcopter or robot car!
+// By increasing beta (GyroMeasError) by about a factor of fifteen, the response time constant is reduced to ~2 sec
+// I haven't noticed any reduction in solution accuracy. This is essentially the I coefficient in a PID control sense; 
+// the bigger the feedback coefficient, the faster the solution converges, usually at the expense of accuracy. 
+// In any case, this is the free parameter in the Madgwick filtering and fusion scheme.
+float beta = sqrt(3.0f / 4.0f) * GyroMeasError;   // compute beta
+float zeta = sqrt(3.0f / 4.0f) * GyroMeasDrift;   // compute zeta, the other free parameter in the Madgwick scheme usually set to a small or zero value
+#define Kp 2.0f * 5.0f // these are the free parameters in the Mahony filter and fusion scheme, Kp for proportional feedback, Ki for integral
+#define Ki 0.0f
+
+#endif avb_mpu9250
+
+void mpu9250_setup();
+
+/*---------------------------------- Lokale Funktionen -----------------------------------------*/
+void writeByte(uint8_t address, uint8_t subAddress, uint8_t data);
+uint8_t readByte(uint8_t address, uint8_t subAddress);
+void readBytes(uint8_t address, uint8_t subAddress, uint8_t count, uint8_t * dest);
+void magcalMPU9250(float * dest1, float * dest2);
+void getMres();
+void getGres();
+void getAres();
+void readAccelData(int16_t * destination);
+void getMres();
+void readMagData(int16_t * destination);
+int16_t readTempData();
+void initAK8963(float * destination);
+void readGyroData(int16_t * destination);
+void initMPU9250();
+void MPU9250SelfTest(float * destination);
+void calibrateMPU9250(float * dest1, float * dest2);
+/*----------------------------------------------------------------------------------------------*/
